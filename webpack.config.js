a1f13e52212d9872cd8138a94f5524c04eae6ddb
Elmer Jose Muñoz Zuñiga <67802793+Ejmz216@users.noreply.github.com>
2023-03-10 15:32:10 -0500

    5_ Deploy Config

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,8 @@ module.exports = {
     entry: './src/index.js', // punto de entrada
     output: { // lugar al que saldrán todos los archivos
         path: path.resolve(__dirname, 'dist'), //en nuestro path, crea la carpeta dist
-        filename: 'bundle.js', // nombre del archivo js resultante
-        publicPath: '/',
+        filename: 'virtual-shop-site/bundle.js', // nombre del archivo js resultante
+        publicPath: '/virtual-shop-site/',
     },
     resolve: { // extensión de archivos a tomar en cuenta
         extensions: ['.js', '.jsx'],
